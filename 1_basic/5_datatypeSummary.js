5d419aca52b3f94b415e55e334074ca54da4be56
Fastas-Duck <160280569+Fastas-Duck@users.noreply.github.com>
2024-06-02 15:36:05 +0000

    datatype summary

---
+++ 1_basic/5_datatypeSummary.js
@@ -0,0 +1,21 @@
+/*
+Data Types:
+
+1. Primitive :  String, Number, Boolean, null, undefined, symbol   [7]
+
+2. Non-Primitive : Array, Objects, Function
+
+JS is dynamic type : we don't need to define the datatype 
+
+*/
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+name: "hitesh",
+age: 22,
+}
+
+const myFunction = function() {
+console.log("Hello world");
+}
