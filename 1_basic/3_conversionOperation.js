6ed097a2783a1a46020d70613b54063ecf3ff729
Fastas-Duck <160280569+Fastas-Duck@users.noreply.github.com>
2024-06-02 14:22:41 +0000

    Operation

--- 1_basic/3_conversionOperation.js
+++ 1_basic/3_conversionOperation.js
@@ -1,5 +1,5 @@
 let score = 33
-console.log(typeof score);
+// console.log(typeof score);
 
 
 /*
@@ -39,7 +39,7 @@ console.log(valueInNumber3);
 
 let loggedIn = "hitesh"
 let boolLoggedIn = Boolean(loggedIn)
-console.log(boolLoggedIn);
+// console.log(boolLoggedIn);
 
 /*
     0 => false , 1 => true
@@ -49,5 +49,37 @@ console.log(boolLoggedIn);
 
 let someNum = 33
 let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// *************************************************************
+//Operations
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Aman"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);               // 12
+console.log(1 + "2");               // 12
+console.log("1" + 2 + 2);           // 122
+console.log(1 + 2 + "2");           // 32
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+console.log(gameCounter++)
+console.log(++gameCounter)
