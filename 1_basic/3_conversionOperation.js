c6c87b93d9bdebc79d10152992ab1999cf0eb392
Fastas-Duck <160280569+Fastas-Duck@users.noreply.github.com>
2024-06-02 13:33:10 +0000

    Conversion dataype

---
+++ 1_basic/3_conversionOperation.js
@@ -0,0 +1,53 @@
+let score = 33
+console.log(typeof score);
+
+
+/*
+    "33" => 33
+    "33abc" => NaN (Not A Number)
+    true => 1, false=> 0
+    null => 0
+    undefined => NaN
+
+let num = "33abc"
+console.log(num);
+let valueInNumber = Number(num)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let num1 = null
+console.log(num1);
+let valueInNumber1 = Number(num1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+let num2 = undefined
+console.log(num2);
+let valueInNumber2 = Number(num2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let num3 = true
+console.log(num3);
+let valueInNumber3 = Number(num3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+
+*/
+
+
+let loggedIn = "hitesh"
+let boolLoggedIn = Boolean(loggedIn)
+console.log(boolLoggedIn);
+
+/*
+    0 => false , 1 => true
+    "xyz" => true
+    "" => false
+*/
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
