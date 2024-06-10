3b37356686219b87530a66034a6a9164a9d0388a
Fastas-Duck <160280569+Fastas-Duck@users.noreply.github.com>
2024-06-10 12:19:36 +0000

    how js work

---
+++ 3_imp/5_JS_Work.js
@@ -0,0 +1,14 @@
+
+JAVASCRIPT EXECUTION AudioContext
+
+
+  JS Code  --> Global Execution Context (used with "this")
+               Functional Execution Context
+               Eval Execution Context
+
+
+JS Code run in 2 phase :
+1. Memory Creation phase
+2. Execution phase
+
+
